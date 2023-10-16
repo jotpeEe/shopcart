cd10dd374135561f839f0f9dcd27e03a1b3300ef
Maciej Klos <maciekklospl@gmail.com>
2023-10-16 09:53:17 +0200

    utils: setup prettier husky lint-staged shadcn

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+    extends: ['next/core-web-vitals', 'prettier'],
+    plugins: ['prettier', 'unused-imports'],
+    rules: {
+        'prettier/prettier': 'error',
+        'import/order': [
+            'error',
+            {
+                groups: ['builtin', 'external', 'internal'],
+                pathGroups: [
+                    {
+                        pattern: 'react',
+                        group: 'external',
+                        position: 'before',
+                    },
+                ],
+                pathGroupsExcludedImportTypes: ['react'],
+                'newlines-between': 'always',
+                alphabetize: {
+                    order: 'asc',
+                    caseInsensitive: true,
+                },
+            },
+        ],
+        'import/newline-after-import': ['error'],
+        'sort-imports': [
+            'error',
+            {
+                ignoreCase: false,
+                ignoreDeclarationSort: true, // don"t want to sort import lines, use eslint-plugin-import instead
+                ignoreMemberSort: false,
+                memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+                allowSeparatedGroups: true,
+            },
+        ],
+    },
+};
