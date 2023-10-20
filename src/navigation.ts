816064cc3779cdb02b74a5e64e8fe0fadd124cfd
Maciej Klos <maciekklospl@gmail.com>
2023-10-20 15:11:04 +0200

    feature: add LocaleSwitcher for next-intl

--- src/navigation.ts
+++ src/navigation.ts
@@ -11,6 +11,7 @@ export const pathnames = {
     // If all locales use the same pathname, a
     // single external path can be provided.
     '/': '/',
+    '/auth': '/auth',
 } satisfies Pathnames<typeof locales>;
 
 export const { Link, redirect, usePathname, useRouter } =
