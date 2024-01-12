66193d74500f9948184935b452c60431de351411
Maciej Klos <maciekklospl@gmail.com>
2024-01-12 10:14:13 +0000

    feat(*): setup eslint, prettier, husky, lint-staged

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'next/core-web-vitals',
+        'prettier',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    plugins: ['prettier', 'unused-imports', '@typescript-eslint'],
+    rules: {
+        'unused-imports/no-unused-imports-ts': 'error',
+        'prettier/prettier': 'error',
+        '@typescript-eslint/consistent-type-imports': [
+            'error',
+            {
+                prefer: 'type-imports',
+                fixStyle: 'inline-type-imports',
+            },
+        ],
+        'import/order': [
+            'error',
+            {
+                groups: ['builtin', 'external', 'internal'],
+                pathGroups: [
+                    {
+                        pattern: 'react',
+                        group: 'external',
+                        position: 'before',
+                    },
+                ],
+                pathGroupsExcludedImportTypes: ['react'],
+                'newlines-between': 'always',
+                alphabetize: {
+                    order: 'asc',
+                    caseInsensitive: true,
+                },
+            },
+        ],
+        'import/newline-after-import': ['error'],
+        'sort-imports': [
+            'error',
+            {
+                ignoreCase: false,
+                ignoreDeclarationSort: true, // don"t want to sort import lines, use eslint-plugin-import instead
+                ignoreMemberSort: false,
+                memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+                allowSeparatedGroups: true,
+            },
+        ],
+    },
+};
